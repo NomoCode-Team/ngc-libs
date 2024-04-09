2e6f57713c41af0ea222eac1d0af7bea22c0ba28
NomoCode <hello@nomocode.io>
2024-04-08 17:22:18 -0700

    Fix tailwind utils config content filepath

--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -4,9 +4,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [
     require('@tailwindcss/aspect-ratio'),
